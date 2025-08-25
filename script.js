3a873949689ae4389564a8cc3f5698051689f62e
Rimsha Ejaz <RIMSHAEJAZ3210@GMAIL.COM>
2025-08-25 23:43:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Scroll animations
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = { threshold: 0.2 };
+
+const appearOnScroll = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
